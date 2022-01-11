a9adfcb409a3200a7202d772426a0b6f47cd261d
mosahil228 <mosahil228@gmail.com>
2022-01-11 21:55:42 +0530

    one commit

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@
 const express=require('express');
 const path=require("path");
 const hbs=require("hbs");
-const port=8000;
+const port=process.env.PORT || 8000;
 const app=express();
 
 // public static path
